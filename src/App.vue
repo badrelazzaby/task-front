56f02ec65054a8ef5b48518a4da03c7b079b06bd
badrelazzaby <badr.azzaby411@gmail.com>
2021-06-17 10:25:36 +0100

    download file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+      crossorigin="anonymous"
+    />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <center><a href="#" @click="XMLdownload()"> Download </a></center> <br> -->
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" />
+    <button @click="sendMessage()">send message</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import * as io from "socket.io-client";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      socket: io("http://localhost:5000"),
+      users: [],
+      message: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    XMLdownload() {
+      axios({
+        url: "http://localhost:5000/api/etudiant/getXML",
+        method: "GET",
+        responseType: "blob",
+      }).then((response) => {
+        var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+        var fileLink = document.createElement("a");
+
+        fileLink.href = fileURL;
+        fileLink.setAttribute("download", "TEST.xml");
+        document.body.appendChild(fileLink);
+
+        fileLink.click();
+      });
+    },
+    sendMessage() {
+      this.socket.emit("message", this.message);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
